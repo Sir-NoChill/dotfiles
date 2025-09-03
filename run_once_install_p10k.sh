8702dae9ee091bf1a7614de72d909b015029e17d
Ayrton Chilibeck <git@blobfish.icu>
2025-09-02 18:01:25 -0600

    fix: p10k check and oh-my-zsh

--- run_once_install_p10k.sh
+++ run_once_install_p10k.sh
@@ -1,1 +1,16 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+#!/bin/bash
+
+# Check if oh-my-zsh is installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Check if powerlevel10k is already installed
+P10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$P10K_DIR" ]; then
+    echo "Installing powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+else
+    echo "powerlevel10k is already installed"
+fi
